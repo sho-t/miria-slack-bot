212ded3557e45d373c139de25f0b87b36d943cb0
sho-t <aznecosann@gmail.com>
2017-05-13 23:43:47 +0900

    add rules & fix format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,30 @@
 module.exports = {
-    "extends": "google",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": 6,
-        "ecmaFeatures": {
-          "experimentalObjectRestSpread": true
-        }
-    },
-    "plugins": [ "googleappsscript" ],
-    "env": {
-        "googleappsscript/googleappsscript": true,
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "settings": {
-        "ecmascript": 6
+  "extends": "google",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
     }
+  },
+  "plugins": [ "googleappsscript" ],
+  "env": {
+    "googleappsscript/googleappsscript": true,
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "settings": {
+    "ecmascript": 6
+  },
+  "rules": {
+    "max-len": [
+      2,
+      {
+        "code": 120,
+        "tabWidth": 4,
+        "ignoreUrls": true
+      }
+    ]
+  }
 };
